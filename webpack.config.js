cbf4417fa7ac538b93a9e821dda7964fe98ae86a
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-01 20:45:51 -0400

    Adding fonts and images to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'main.js',
+		assetModuleFilename: 'assets/images/[hash][ext][query]',
 	},
 	resolve: {
 		extensions: ['.js'],
@@ -24,6 +26,24 @@ module.exports = {
 				test: /\.css|.styl$/i,
 				use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
 			},
+			{
+				test: /\.png/,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 10000,
+						mimetype: 'application/font-woff',
+						name: '[name].[ext]',
+						outputPath: './assets/fonts',
+						publicPath: './assets/fonts',
+						esModule: false,
+					},
+				},
+			},
 		],
 	},
 	plugins: [
@@ -33,5 +53,13 @@ module.exports = {
 			filename: './index.html',
 		}),
 		new MiniCssExtractPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src', 'assets/images'),
+					to: 'assets/images',
+				},
+			],
+		}),
 	],
 }
